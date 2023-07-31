c7cc01051f0b9d02ba2e67c7a26070539d1dca69
Nina Ramšak <nina.ramsak23@gmail.com>
2023-07-31 21:39:36 +0200

    added Woodz header and albums XD

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@
 import streamlit
 
 streamlit.title('My KPOP Library')
+
+streamlit.header('WOODZ')
+
+streamlit.text('06.2020 - EQUAL')
+streamlit.text('11.2020 - WOOPS')
+streamlit.text('03.2021 - SET')
+streamlit.text('10.2021 - Only Lovers Left')
+streamlit.text('04.2022 - Colorful Trauma')
+streamlit.text('04.2023 - OO-LI')
