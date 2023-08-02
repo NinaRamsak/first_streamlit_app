c88c84dda937f1fae743c46f8494b526cfac0499
Nina Ramšak <nina.ramsak23@gmail.com>
2023-08-02 20:56:31 +0200

    format json into dataframe/table

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,16 @@ streamlit.header('KEY')
 streamlit.text('09.2021 - Bad Love')
 streamlit.text('08.2022 - Gasoline 🍇')
 
+####################################################################
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response.json())
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show original json from fruityvice as a dataframe - table
+streamlit.dataframe(fruityvice_normalized)
+
+####################################################################
 streamlit.header('Fruit Smoothie')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
