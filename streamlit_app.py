ff90a6fefbb6e3a37b1716a28092efbf0152a544
Nina Ramšak <nina.ramsak23@gmail.com>
2023-08-02 07:42:30 +0200

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,7 @@ streamlit.text('09.2021 - Bad Love')
 streamlit.text('08.2022 - Gasoline 🍇')
 
 streamlit.header('Fruit Smoothie')
-streamlit.dataframe(my_fruit_list)
-
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+streamlit.dataframe(my_fruit_list)
