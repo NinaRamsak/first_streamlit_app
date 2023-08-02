508456ef17e644b3910d0fa2311b97779c2a5abd
Nina Ramšak <nina.ramsak23@gmail.com>
2023-08-02 20:54:14 +0200

    added json

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.text('08.2022 - Gasoline 🍇')
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.header('Fruit Smoothie')
 
