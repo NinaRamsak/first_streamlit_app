3ef9a8efed2f1cc33d48a28c816d307d89b238df
Nina Ramšak <nina.ramsak23@gmail.com>
2023-08-02 07:30:29 +0200

    add pandas and read fruit info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My KPOP Library')
 
@@ -14,3 +17,6 @@ streamlit.text('04.2023 - OO-LI 🥭')
 streamlit.header('KEY')
 streamlit.text('09.2021 - Bad Love')
 streamlit.text('08.2022 - Gasoline 🍇')
+
+streamlit.header('Fruit Smoothie')
+streamlit.dataframe(my_fruit_list)
